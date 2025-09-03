03432838ffdbe80473abf0020793fa3537d9164d
felipeIotti <felipeizago@hotmail.com>
2025-09-03 17:23:47 -0300

    removendo submódulos e adicionando projetos como pastas normais

---
+++ purchases/src/http/authorization/authorization.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { auth } from 'express-oauth2-jwt-bearer';
+import { promisify } from 'node:util';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  private AUTH0_AUDIENCE: string;
+  private AUTH0_DOMAIN: string;
+
+  constructor(private configService: ConfigService) {
+    this.AUTH0_AUDIENCE = this.configService.get('AUTH0_AUDIENCE') ?? '';
+    this.AUTH0_DOMAIN = this.configService.get('AUTH0_DOMAIN') ?? '';
+  }
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const httpContext = context.switchToHttp();
+    const req = httpContext.getRequest();
+    const res = httpContext.getResponse();
+
+    const checkJWT = promisify(
+      auth({
+        audience: this.AUTH0_AUDIENCE,
+        issuerBaseURL: this.AUTH0_DOMAIN,
+      }),
+    );
+    try {
+      await checkJWT(req, res);
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+  }
+}
